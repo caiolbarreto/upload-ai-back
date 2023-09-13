ffecd72f5cff7b4e3b3daed7faf78e62f5b451ee
Caio Barreto <caiobarreto_28@hotmail.com>
2023-09-13 18:30:46 -0300

    upload video and get prompts routes done

---
+++ prisma/migrations/20230913020642_create_videos_and_prompts/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "videos" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "transcription" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "prompts" (
+    "id" TEXT NOT NULL PRIMARY KEY
+);
