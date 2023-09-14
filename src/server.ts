f5372cdd29406046d70ba4e3ef9d917de298847f
Caio Barreto <caiobarreto_28@hotmail.com>
2023-09-14 17:13:30 -0300

    create the completion route and allow cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
+import { fastifyCors } from '@fastify/cors'
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletion } from "./routes/generate-ai-completion";
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletion)
 
 app.listen({
   port: 3333
